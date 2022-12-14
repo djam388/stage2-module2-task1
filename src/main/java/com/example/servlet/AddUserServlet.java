5f91ec77d68567fdc5d8dff9085a009a0915c0de
djam388 <djam388@hotmail.com>
2022-12-14 17:43:29 +0500

    Attempt #1

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,43 @@
 package com.example.servlet;
 
-public class AddUserServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
+    User user = new User();
+    public void init(){
+
+        user.setFirstName("John");
+        user.setLastName("Black");
+        Warehouse.getInstance().addUser(user);
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req,HttpServletResponse res)
+            throws IOException
+    {
+        res.setContentType("text/html");//setting the content type
+        PrintWriter pw=res.getWriter();//get the stream to write the data
+
+//writing html in the stream
+        pw.println("<html><body>");
+        pw.println("Added user(s) to Warehouse:");
+        pw.println("<br>");
+        for (User addedUser : Warehouse.getInstance().getUsers()) {
+            pw.println(addedUser.getFirstName() + " " + addedUser.getLastName());
+            pw.println("<br>");
+        }
+        pw.println("</body></html>");
+
+
+        pw.close();//closing the stream
+    }
 }
